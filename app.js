8624655ec8d4c93cba9bc92e37693b2bd576f729
Charles Ybarra <59151304+CharlieArray@users.noreply.github.com>
2020-09-25 17:58:32 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+(function(){
+
 // Function for Quiz Visualization and Output of Quizzes/Answers
 function buildQuiz(){
   //stores HTML output
@@ -22,8 +24,10 @@ function buildQuiz(){
 
     // adds the question and answers to the output
     output.push(
-      `<div class="question" > ${currentQuestion.question} </div>
-      <div class="answers" > ${answers.join('')} </div>`
+      `<div class="slide">
+        <div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join("")} </div>
+      </div>`
     );
   }
 );
@@ -53,7 +57,7 @@ function showResults(){
       numCorrect++;
 
     //styling to make correct answers green
-    answerContainers[questionNumber].style.color = 'green';
+    answerContainers[questionNumber].style.color = 'light green';
     }
 
   //else if statement if wrong answer 
@@ -67,22 +71,40 @@ function showResults(){
 resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
 }
 
+function showSlide(n) {
+  slides[currentSlide].classList.remove('active-slide');
+  slides[n].classList.add('active-slide');
+  currentSlide = n;
+  if(currentSlide === 0){
+    previousButton.style.display = 'none';
+  }
+  else{
+    previousButton.style.display = 'inline-block';
+  }
+  if(currentSlide === slides.length-1){
+    nextButton.style.display = 'none';
+    submitButton.style.display = 'inline-block';
+  }
+  else{
+    nextButton.style.display = 'inline-block';
+    submitButton.style.display = 'none';
+  }
+}
+
+function showNextSlide() {
+  showSlide(currentSlide + 1);
+}
+
+function showPreviousSlide() {
+  showSlide(currentSlide - 1);
+}
+
+
 //HTML elements stored as variables//
-const quizContainer = document.getElementById('quiz')
-const resultsContainer = document.getElementById('results')
-const submitButton = document.getElementById('submit')
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
 const myQuestions = [
-  {
-    question: "What is the primary difference between the S&P 500 index vs DOW Jones Industrial (DJI) index?",
-    answers: {
-      a: "DOW Jones stocks are more important because the index price is larger than S&P 500",
-      b: "S&P500 stocks are more important",
-      c: "S&P500 is market cap weighted index vs DJI which is price weighted index",
-      d: "DOW Jones stocks are more important"
-    },
-    correctAnswer: "c"
-  },
-    
   {
     question: "Like real estate, gold and cash, stocks are _____ and owning stock means you are _____ of the company.",
     answers: {
@@ -92,6 +114,18 @@ const myQuestions = [
       d: "all the above"
     },
     correctAnswer: "b"
+  },
+    
+
+  {
+    question: "What is the primary difference between the S&P 500 index vs DOW Jones Industrial (DJI) index?",
+    answers: {
+      a: "DOW Jones stocks are more important because the index price is larger than S&P 500",
+      b: "S&P500 stocks are more important",
+      c: "S&P500 is market cap weighted index vs DJI which is price weighted index",
+      d: "DOW Jones stocks are more important"
+    },
+    correctAnswer: "c"
   },
     
   {
@@ -152,9 +186,21 @@ const myQuestions = [
 //Call Quiz Function right away//
 buildQuiz();
 
+ // Pagination
+ const previousButton = document.getElementById("previous");
+ const nextButton = document.getElementById("next");
+ const slides = document.querySelectorAll(".slide");
+ let currentSlide = 0;
+
+//Shows first slide 
+showSlide(currentSlide);
+
+// Event listeners
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
 submitButton.addEventListener('click', showResults);
 
-
+})();
 
 
 
