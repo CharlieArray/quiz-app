aa8c0baf9deb4cbcebb92160ca873d1082c87504
Charles Ybarra <59151304+CharlieArray@users.noreply.github.com>
2020-10-08 00:55:05 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const store = [
   {
     question: "What is the primary difference between the S&P 500 index vs DOW Jones Industrial (DJI) index?",
     answers: {
-      a: "the DOW Jones stocks are more important because the index price is larger than S&P 500",
+      a: "the DOW Jones is more important because the index price is larger than S&P 500",
       b: "the S&P500 stocks are more important",
       c: "the S&P500 is market cap weighted index vs DJI which is price weighted index",
       d: "the DOW Jones stocks are more important"
@@ -53,19 +53,21 @@ const store = [
   },
     
   {
-    question: "Does a stock with a low share price mean it has more potential upside to increase in value?",
+    question: "The Securities and Exchange Commission (SEC), classifies stocks under ____ as penny stocks.",
     answers: {
-      a: "yes, most likely",
-      b: "no, not always"
+      a: "$5.00",
+      b: "$1.00",
+      c: "$0.99",
+      d: "$10.00"
     },
-    correctAnswer: "b"
+    correctAnswer: "a"
   },
 
   {
     question: "When investing over a long period of time, what is the most successful strategy?",
     answers: {
       a: "buying stock all at once",
-      b: "dollar cost averaging (DCA), meaning you buy a fixed dollar amount of stock over x-period of time",
+      b: "dollar cost averaging (DCA), meaning buying fixed dollar amounts of stock over time",
       c: "buying penny stocks",
       d: "never investing",
     },
@@ -73,7 +75,7 @@ const store = [
   },
 
   {
-    question: "Market cap is the ______ multiplied by the _____ to be traded by the public.",
+    question: "Market cap is the ______ multiplied by <br> the _____ to be traded by the public.",
     answers: {
       a: "company, value",
       b: "dollars, value",
@@ -187,8 +189,6 @@ $('main').prepend(welcomeString);
   quizContainer.innerHTML = output.join('');
 }
 
-//Render Quiz Function right away//
-//renderQuiz();
 
 // Function to calculate results of Quiz
 function showResults(){
@@ -245,10 +245,12 @@ function showSlide(currentSlide) {
 
   //Displays question number "x" out of "y" questions in Quiz
   $('#question_number').text("Question "+(currentSlide+1)+" out of 7");
+
   
-  slides[currentSlide].classList.remove('active-slide');
   slides[currentSlide].classList.add('active-slide');
- 
+  slides[currentSlide + 1].classList.remove('active-slide');
+  
+
   // if on first slide hide previous button
   if(currentSlide === 0){
     previousButton.style.display = 'none';
@@ -277,7 +279,7 @@ function showNextSlide() {
 
 //Function to Show Previous Slide 
 function showPreviousSlide() {
-  currentSlide = currentSlide -1;
+  currentSlide = currentSlide - 1;
   showSlide(currentSlide);
   console.log('`showPreviousSlide` ran');
 }
@@ -371,37 +373,32 @@ The following requirements cover what the app must do, from the user's perspecti
     COMPLETE-Users should be prompted through a series of at least 5 multiple choice questions that they can answer.
         COMPLETE-Users should be asked questions 1 after the other.
         COMPLETE-Users should only be prompted with 1 question at a time.
-    NEED HELP -Users should not be able to skip questions.
+   1) NEED HELP -Users should not be able to skip questions.
     COMPLETE-Users should also be able to see which question they're on (for instance, "7 out of 10") and their current score ("5 correct, 2 incorrect").
-    -NEED HELP Upon submitting an answer, users should:
+   2) -NEED HELP Upon submitting an answer, users should:
         -receive textual feedback about their answer. If they were incorrect, they should be told the correct answer.
         -be moved onto the next question (or interact with an element to move on).
-    -Users should be shown their overall score at the end of the quiz. In other words, how many questions they got right out of the total questions asked.
-    -Users should be able to start a new quiz.
+   3) -Users should be shown their overall score at the end of the quiz. In other words, how many questions they got right out of the total questions asked.
+   4) -Users should be able to start a new quiz.
         function startNewQuiz(){
           -define variables if variables/objects outside of function
           -conditional statement: if currentquestion == store.length && user hits next button, {1) unhide restartQuiz button) }
           } >>if user clicks on restartQuiz button, an event listener is triggered that executes a callback function that refreshes the page. 
 
-    NEED HELP -Users should not be able to skip questions.
-
-    NEED HELP Upon submitting an answer, users should:
-        -receive textual feedback about their answer. If they were incorrect, they should be told the correct answer.
-        -be moved onto the next question (or interact with an element to move on).
         
 Technical requirements
 
 Your quiz app must:
 
-    -Include a render() function, that conditionally regenerates the view each time the store is updated.
+    COMPLETE- Include a render() function, that conditionally regenerates the view each time the store is updated.
     -Include single-purpose template generation functions.
-    DONE?-Include single-purpose event handler functions.
-    -Call all functions from a jQuery initializing function.
-    -NOT add additional HTML elements to the boilerplate code's index.html file 
+    COMPLETE -Include single-purpose event handler functions.
+    COMPLETE -Call all functions from a jQuery initializing function.
+   5) -NOT add additional HTML elements to the boilerplate code's index.html file 
         (you may add attributes, e.g., classes and ids, to the existing HTML elements, or link stylesheets or additional scripts if necessary).
-    -Render answer choices in a <form>.
-    -Use semantic HTML, along with CSS and jQuery.
-    -Follow a11y best practices.
+   6) -Render answer choices in a <form>.
+   7) -Use semantic HTML, along with CSS and jQuery.
+   8) -Follow a11y best practices.
     -Be fully usable by keyboard (which will be easy enough if you start with a form).
     -Use responsive design.
         Refer back to the previous checkpoints on responsive design and forms for any help with the HTML/CSS materials.
